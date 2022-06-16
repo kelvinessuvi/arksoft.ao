ca8cd9fcd5e036f80ef181b842056feb2f9b1695
Kelvin Domingos <kelvindomingos20@hotmail.com>
2022-06-16 12:20:37 +0100

    a

--- page.php
+++ page.php
@@ -46,6 +46,8 @@
 		else{
 			throw new Exception("Não foi possível realizar o upload do arquivo selecionado.");
 		}
+        
+        include 'includes/footer.php';
     }
 
     if($_GET['page'] == "resocial"){
