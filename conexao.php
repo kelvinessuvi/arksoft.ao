26969ab7cfc846318c73a673e02546e892375efa
Kelvin Domingos <kelvindomingos20@hotmail.com>
2022-06-16 10:23:19 +0100

    a

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $host = "localhost";
+    $user = "filoneua_admin";
+    $pass ="@Luanda-923";
+    $bd = "filoneua_arksoftbd";
+
+    $conexao = mysqli_connect($host,$user,$pass,$bd);
+
+
+
+?>
