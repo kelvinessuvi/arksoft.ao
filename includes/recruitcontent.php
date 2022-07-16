3ec4d84e03db4bc9d82a9062133c2a6eb80b87d2
Kelvin Domingos <kelvindomingos20@hotmail.com>
2022-07-16 13:10:22 +0100

    a

--- includes/recruitcontent.php
+++ includes/recruitcontent.php
@@ -71,7 +71,7 @@
 
                     <div class="field col-lg-6 col-md-6 col-sm-12 col-xs-12">
                       <label>Anexe o seu CV</label>
-                      <input class="form_custom" name="cv_candidato" accept=".pdf, .docx" type="file">
+                      <input class="form-control" name="cv_candidato" accept=".pdf, .docx" type="file">
                     </div>
                     <div style="margin-bottom:90px;" class="center"><button class="btn btn-primary">Candidatar-se</button></div>
 
